6b04e10827a1f9f5fbe0cc6bc941fd7b6375c200
Eric J <eric@Erics-Laptop-2.local>
2025-07-31 23:21:44 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+//end section
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
+
+//scroll magic
+const controller = new ScrollMagic.Controller();
+
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+    duration: 2000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+
+//text animation
+
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 2000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+.addTo(controller); 
+
+scene2.on("progress", function(e) {
+    text.style.opacity = 1 - e.progress;
+});
+
+//video animation
+let accelamount = 0.5;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = delay;
+}, 33.3);
